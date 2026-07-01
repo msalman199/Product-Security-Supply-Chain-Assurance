18b8abee808612038a1a0a37cf7f81d1d940f46d
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-30 20:19:13 -0700

    Create verify_boofuzz.py

---
+++ Fuzz Network Protocols with Boofuzz/boofuzz-lab/verify_boofuzz.py
@@ -0,0 +1,21 @@
+# verify_boofuzz.py
+#!/usr/bin/env python3
+
+import boofuzz
+import sys
+
+def verify_installation():
+    """
+    Verify Boofuzz installation and display version.
+    
+    TODO: Import boofuzz module
+    TODO: Print version information
+    TODO: Test basic Session creation
+    TODO: Return True if successful, False otherwise
+    """
+    pass
+
+if __name__ == "__main__":
+    # TODO: Call verify_installation()
+    # TODO: Exit with appropriate status code
+    pass
