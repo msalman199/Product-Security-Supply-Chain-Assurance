36516b5e659c2607b1b80952378b2371c2f84eaa
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-30 18:58:11 -0700

    Create custom_client.py

---
+++ Reverse-Engineer Undocumented Protocols/protocol_lab/custom_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import socket
+
+def connect_to_server():
+    """
+    TODO: Connect to server on localhost:8888
+    TODO: Receive and display messages
+    TODO: Handle connection errors gracefully
+    """
+    pass
+
+if __name__ == "__main__":
+    connect_to_server()
